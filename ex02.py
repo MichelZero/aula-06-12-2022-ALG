f8065698dcfb0d0ed8361d2327b160abd02b19fb
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-06 17:17:36 -0300

    up

--- ex02.py
+++ ex02.py
@@ -12,22 +12,24 @@ lista1 = []
 # indices: 0 1 2 3 e 4
 # lista[0] = valor
 
-for i in range(5):
+for i in range(5): # 0, 1, 2, 3, 4
   # print(i)
   valor = int(input(f"informe o valor[{i}]: "))
-  lista1.append(valor)
+  lista1.append(valor) # adiciona o valor no fim da lista
 
-lista1.reverse()
-print(lista1)
+lista1.reverse() # inverte a ordem da lista 
+print(lista1) # imprime a lista invertida
 
 # imprimir cada número
 for i in range(5):
-  print(f"{i} -> {lista1[i]}")
+  print(f"{i} -> {lista1[i]}") # imprime o indice e o valor do indice
   
 """ lista2 = lista1
 lista2[0] = 50
 print(lista1)
 print(lista2) """
 
-l2 = lista1[::-1]
+l2 = lista1[::-1] # inverte a ordem da lista 
 print(l2)
+
+print("fim do programa")
