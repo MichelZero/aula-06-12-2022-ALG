ebe492ed39790861589509008c5f0733c06c8da1
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-06 17:16:32 -0300

    up

--- ex04.py
+++ ex04.py
@@ -6,12 +6,13 @@
 # 4 – ler uma lista de 5 números e imprimir o menor e
 # maior valor.
 
-lista1 = []
-for i in range(5):
-  # print(i)
-  valor = int(input(f"informe o valor[{i}]: "))
-  lista1.append(valor)
+lista1 = [] # lista vazia
+for i in range(5):  # 0, 1, 2, 3, 4
+  # print(i) # 0, 1, 2, 3, 4
+  valor = int(input(f"informe o valor[{i}]: ")) 
+  lista1.append(valor) # adiciona o valor no fim da lista 
 
-maior = max(lista1)
-menor = min(lista1)
-print(f"Maior ={maior}, Menor={menor}")
+maior = max(lista1) # maior valor da lista
+menor = min(lista1) # menor valor da lista
+print(f"Maior ={maior}, Menor={menor}") # imprime o maior e menor valor da lista
+print("fim do programa")
