172009cd92276d160df5ac23481bdd9eb33fa75f
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-06 17:14:15 -0300

    up

--- ex05.py
+++ ex05.py
@@ -1,16 +1,23 @@
-#
-import pywhatkit as kit
-import time
-from datetime import datetime as dt
+## autor:
+# Michel silva
 
-quantidade = int(input("informe a quantidade: "))
-msg = input("informe a mensagem: ")
+# data: 06/12/2022
 
-contatos = ['+5583982173125']
+# enviar mensagem para uma lista de números pelo WhatsApp
 
-i = 0
-while i < quantidade:
-  kit.sendwhatmsg(contatos[0], msg, dt.now().hour, dt.now().minute + 1)
-  # kit.sendwhatmsg(contatos[0], msg, 1, 30) # 1:30
-  time.sleep(5)
-  contatos.pop(0)
+
+import pywhatkit as kit #  pip install pywhatkit ou pip3 install pywhatkit
+import time #  pip install time ou pip3 install time
+from datetime import datetime as dt # pip install datetime ou pip3 install datetime
+
+quantidade = int(input("informe a quantidade: "))  # quantidade de contatos que serão enviados a mensagem
+msg = input("informe a mensagem: ") # mensagem que será enviada
+
+contatos = ['+5583982173125']  # lista de contatos que serão enviados a mensagem
+
+i = 0  # contador
+while i < quantidade: # enquanto o contador for menor que a quantidade de contatos
+  kit.sendwhatmsg(contatos[0], msg, dt.now().hour, dt.now().minute + 1) # envia a mensagem para o primeiro contato da lista e aguarda 1 minuto para enviar a próxima mensagem
+  # kit.sendwhatmsg(contatos[0], msg, 1, 30) # 1:30 da manhã  
+  time.sleep(5) # aguarda 5 segundos para enviar a próxima mensagem 
+  contatos.pop(0) # remove o primeiro contato da lista para enviar a próxima mensagem
