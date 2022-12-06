24b093c7b6d7adb05e7b3aba8db001a23cfc6f1d
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-06 17:19:12 -0300

    up

--- ex01.py
+++ ex01.py
@@ -9,15 +9,17 @@
 
 #
 # ler uma lista de 5 número inteiros
-lista = [] 
+lista = []  # lista vazia
 # indices: 0 1 2 3 e 4
 # lista[0] = valor
 
 for i in range(5):
   # print(i)
   valor = int(input(f"informe o valor[{i}]: "))
-  lista.append(valor)
+  lista.append(valor) # adiciona o valor no fim da lista
 
 # imprimir cada número
 for i in range(5):
-  print(f"{i} -> {lista[i]}")
+  print(f"{i} -> {lista[i]}") # imprime o indice e o valor do indice
+  
+print("fim do programa")
