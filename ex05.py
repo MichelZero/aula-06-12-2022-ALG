4206fdbe31b34238732a06b8be6ce6f308e3a7d7
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-06 11:08:10 -0300

    up

--- ex05.py
+++ ex05.py
@@ -11,5 +11,6 @@ contatos = ['+5583982173125']
 i = 0
 while i < quantidade:
   kit.sendwhatmsg(contatos[0], msg, dt.now().hour, dt.now().minute + 1)
+  # kit.sendwhatmsg(contatos[0], msg, 1, 30) # 1:30
   time.sleep(5)
   contatos.pop(0)
