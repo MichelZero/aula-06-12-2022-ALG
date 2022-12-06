841722dc19e9900dc4f30af65c8d987e1b43116f
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-06 17:18:26 -0300

    up

--- ex03.py
+++ ex03.py
@@ -4,11 +4,12 @@
 # data: 06/12/2022
 #
 # 3 – ler uma lista de 4 notas e em seguida mostra as notas e a média
-listaNotas = []
-for i in range(4):
+listaNotas = [] # lista vazia 
+for i in range(4): # 0, 1, 2, 3
   nota = int(input('informe a nota:' ))
-  listaNotas.append(nota)
+  listaNotas.append(nota) # adiciona a nota no fim da lista 
 
-print(listaNotas)
-media = sum(listaNotas) / len(listaNotas)
-print(f"média = {media}")
+print(listaNotas) # imprime a lista de notas 
+media = sum(listaNotas) / len(listaNotas) # calcula a média das notas 
+print(f"média = {media}") # imprime a média das notas
+print("fim do programa")
